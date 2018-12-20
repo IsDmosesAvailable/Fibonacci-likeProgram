69dbc382060dc728ae43205f8be3d818f4669ef4
Donald <dmoses20@scienceleadership.org>
2018-12-20 10:24:37 -0500

    hjbh

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,16 @@ public class Main{
 
 
     public static void main(String[] args) {
-        //for (int i = 1; i <= 8; i++) {
-            FibonacciLike fib = new FibonacciLike(3, 5, 8);
-            // 3 5 8 13 21 34 55 89
-            fib.run();
-        }
+        FibonacciLike fib = new FibonacciLike(1, 2, 8);
+        // 3 5 8 13 21 34 55 89...
+        fib.run();
+
+        FibonacciLike fib2 = new FibonacciLike(3, 4, 8);
+        // 3 5 8 13 21 34 55 89...
+        fib2.run();
+
+        FibonacciLike fib3 = new FibonacciLike(-2, 6, 8);
+        // 3 5 8 13 21 34 55 89...
+        fib3.run();
     }
-//}
+}
